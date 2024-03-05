275fde7534919abbe5858ca703c1353036f491ac
Muhammed Hatay <46846228+trappigs@users.noreply.github.com>
2024-03-05 11:22:51 +0300

    Add project files.

---
+++ stok-takip-1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace stok_takip_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
